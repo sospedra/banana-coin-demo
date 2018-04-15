ae42e3a1b7810426455aad599dbdcef6afa5f5ab
Rubén Sospedra <r.sospedra@mytaxi.com>
2018-04-15 18:50:57 +0200

    Add blueprint for banana-coin

--- src/banana-coin/index.js
+++ src/banana-coin/index.js
@@ -2,8 +2,9 @@ const { createRouter } = require('../services/router')
 const { composeBlueprint } = require('../services/blueprint')
 const v1 = require('./routes/version-1')
 const routes = require('./routes')
+const { PATH } = require('./constants')
 
-module.exports.path = '/banana-coin'
+module.exports.path = PATH
 module.exports.current = createRouter(routes)
 module.exports.blueprint = composeBlueprint(routes)
 module.exports.v1 = createRouter(v1)
--- src/banana-coin/routes/index.js
+++ src/banana-coin/routes/index.js
@@ -3,6 +3,7 @@ const { chunk, get } = require('lodash')
 const repository = require('../../services/repository')(__dirname, '../db.json')
 const { promisify } = require('../../services/router')
 const { getPageFromQuery, createLinks, createJAR } = require('../../services/jsonapi')
+const { PATH } = require('../constants')
 
 const STATUS_OPEN = 'open'
 const STAUTS_CLOSE = 'close'
@@ -26,9 +27,28 @@ module.exports.getAll = promisify(async (req, res) => {
 module.exports.getAll.verb = 'get'
 module.exports.getAll.path = '/'
 module.exports.getAll.blueprint = `
-  # GET /banana-coin-sexy-lady
-  + Response 200 (application/json)
-      Hello World!
+## GET ${PATH}/{?number&size}
++ Request (application/json)
+  + Parameters
+    + number (number) - Current page
+      + Default: 0
+    + size (number) - Number of coins per page
+      + Default: 1
++ Response 200 (application/json)
+  + Body
+    {
+      data: {
+        type: 'coins',
+        attributes: [{
+          id: 0,
+          name: 'coin',
+          status: 'open',
+          value: 1337
+        }]
+      },
+      meta: { lastPage: 'Link' },
+      links: { self: 'Link', prev: 'Link', next: 'Link', first: 'link', last: 'link' }
+    }
 `
 
 /**
@@ -43,6 +63,22 @@ module.exports.getByID = promisify(async (req, res) => {
 
 module.exports.getByID.verb = 'get'
 module.exports.getByID.path = '/:id'
+module.exports.getByID.blueprint = `
+## GET ${PATH}/:id
++ Response 200 (application/json)
+  {
+    data: {
+      type: 'coin',
+      id: 13,
+      attributes: {
+        id: 13,
+        name: 'coin',
+        status: 'open',
+        value: 1337
+      }
+    }
+  }
+`
 
 /**
  * create - Add a new operation given a payload
@@ -58,6 +94,22 @@ module.exports.create = promisify(async (req, res) => {
 
 module.exports.create.verb = 'post'
 module.exports.create.path = '/'
+module.exports.create.blueprint = `
+## POST ${PATH}/
++ Request (application/json)
+  + Body
+    {
+      name: 'coin',
+      value: 1337
+    }
++ Response 201 (application/json)
+  {
+    data: {
+      type: 'coin',
+      id: 1337
+    }
+  }
+`
 
 /**
  * updateByID - Change operation data by id
@@ -67,8 +119,12 @@ module.exports.updateByID = promisify(async (req, res) => {
   return res.sendStatus(204)
 })
 
-module.exports.updateByID.verb = 'put'
+module.exports.updateByID.verb = 'patch'
 module.exports.updateByID.path = '/:id'
+module.exports.updateByID.blueprint = `
+## PATCH ${PATH}/:id
++ Response 204 (application/json)
+`
 
 /**
  * deleteByID - Remove one operation by id
@@ -80,6 +136,10 @@ module.exports.deleteByID = promisify(async (req, res) => {
 
 module.exports.deleteByID.verb = 'delete'
 module.exports.deleteByID.path = '/:id'
+module.exports.deleteByID.blueprint = `
+## DELETE ${PATH}/:id
++ Response 204 (application/json)
+`
 
 /**
  * closeByID - Change operation status to closed by id
@@ -91,3 +151,7 @@ module.exports.closeByID = promisify(async (req, res) => {
 
 module.exports.closeByID.verb = 'put'
 module.exports.closeByID.path = '/close/:id'
+module.exports.closeByID.blueprint = `
+## PATCH ${PATH}/close/:id
++ Response 204 (application/json)
+`
